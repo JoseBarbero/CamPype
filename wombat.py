633de96c413cc91e40a59e5258de7790f25f71a3
JoseBarbero <jabarbero@ubu.es>
2019-10-15 11:19:05 +0200

    Initial commit

---
+++ wombat.py
@@ -0,0 +1,28 @@
+from subprocess import call
+
+def trimmomatic_call():
+    pass
+
+def prinseq_call():
+    pass
+
+def spades_call():
+    pass
+
+def contigs_renamed_shorten():
+    pass
+
+def mlst_call():
+    pass
+
+def abricate_call():
+    pass
+
+def prokka_call():
+    pass
+
+def roary_call():
+    pass
+
+if __name__ == "__main__":
+    pass
