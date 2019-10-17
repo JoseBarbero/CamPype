f9119473a1af440920fcea1c80345e1600822c96
JoseBarbero <jabarbero@ubu.es>
2019-10-17 12:26:36 +0200

    Reading filenames

--- wombat.py
+++ wombat.py
@@ -1,7 +1,26 @@
+import pandas
+import datetime
+import os
 from subprocess import call
 
-def trimmomatic_call():
-    pass
+"""
+Gets every pair of reads on input_files.csv
+
+Returns:
+    list of tuples -- List of tuples each containing forward read file path, reverse read file path and file basename (just the sample number)
+"""
+def read_input_files(indexfile):
+    files_df = pandas.read_csv(indexfile, sep="\t")
+    files_tuples = []
+    for _, row in files_df.iterrows():
+        sample_basename = row["Read1"].split("/")[-1].split("_")[0]
+        files_tuples.append((row["Read1"], row["Read2"], sample_basename))
+    return files_tuples
+
+def trimmomatic_call(inputFile1, inputFile2, phred="-phred33", pairedOutputFile1=None, pairedOutputFile2=None, unpairedOutputFile1=None, unpairedOutputFile2=None, trimfile=None):
+    arguments = ["trimmomatic", "PE", phred, inputFile1, inputFile2, pairedOutputFile1, pairedOutputFile2, unpairedOutputFile1, unpairedOutputFile2, trimfile]
+    
+    call(arguments)
 
 def prinseq_call():
     pass
@@ -25,7 +44,22 @@ def roary_call():
     pass
 
 if __name__ == "__main__":
-    trimmomatic_call()
+
+    # Creates output directory with a timestamp
+    now = datetime.datetime.now()
+    output_folder = "Workflow_OUTPUT_"+str(now.strftime('%Y%m%d_%H%M')+"/")
+    os.mkdir(output_folder)
+
+    # Calls trimmomatic for every row in input_files.csv
+    for sample1, sample2, sample_basename in read_input_files("input_files.csv"):
+        trimmomatic_call(inputFile1=sample1,
+                        inputFile2=sample2,
+                        pairedOutputFile1=output_folder+sample_basename+"_R1_paired.fastq",
+                        pairedOutputFile2=output_folder+sample_basename+"_R2_paired.fastq",
+                        unpairedOutputFile1=output_folder+sample_basename+"_R1_unpaired.fastq",
+                        unpairedOutputFile2=output_folder+sample_basename+"_R2_unpaired.fastq",
+                        trimfile="ILLUMINACLIP:adapters.fa:1:30:11")
+
     prinseq_call()
     spades_call()
     contigs_renamed_shorten()
