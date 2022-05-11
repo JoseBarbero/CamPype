49ad04b534b2674ae2239a69f3b715b4ef5e5d15
Irene <iortega@ubu.es>
2022-05-11 13:36:10 +0200

    Update wombat_config.py
    "out_format" and "out_bad" options are not used in Prinseq so they are removed (should have already been removed)

--- wombat_config.py
+++ wombat_config.py
@@ -16,8 +16,6 @@ config = {
         "trim_qual_right": 25,  # Trim sequence by quality score from the 3'-end with this threshold score. Default: 25
         "trim_qual_window": 20,     # The sliding window size used to calculate quality score by type. To stop at the first base that fails the rule defined, use a window size of 1. Default: 20
         "trim_qual_type": "mean",   # Type of quality score calculation to use. Default: 20
-        "out_format": 3,    # Output format 1 (FASTA only), 2 (FASTA and QUAL), 3 (FASTQ), 4 (FASTQ and FASTA), 5 (FASTQ, FASTA and QUAL) (default: {3})
-        "out_bad": "null"   # Data not passing anyfilter will be ignored
     },  
     "spades": {
         "mode": "--isolate",    # Assembly modes: --isolate (highly recommended for high-coverage isolate and multi-cell Illumina data), --careful (reduce the number of mismatches and short indels, recommended only for assembly of small genomes) or --sc (required for MDA (single-cell) data). Default: --isolate
