a229dc2560965ce8f614d0d53148242822f217aa
Irene <iortega@ubu.es>
2022-07-19 20:22:58 +0200

    Update wombat_config.py

--- wombat_config.py
+++ wombat_config.py
@@ -70,7 +70,7 @@ config = {
         "run_plasmid_prediction": True, #Set this to True or False if you want to search for plasmids or not.
         "abricate": {
             "mincov": 80, # Minimum DNA % coverage for considering a plasmid as present. Default: 80
-            "minid": 80 # Minimum DNA % identity for considering a plasmid as present. Default: 80
+            "minid": 60 # Minimum DNA % identity for considering a plasmid as present. Default: 80
         }
     },
     "run_variant_calling": True, # Set this to True or False if you want to run the variant calling step
