f8b134370c8997ba121d13975674343d257d60f6
Irene <iortega@ubu.es>
2022-07-19 08:41:11 +0200

    Update wombat.py
    Line 1925: indentation error

--- wombat.py
+++ wombat.py
@@ -1922,11 +1922,11 @@ if __name__ == "__main__":
                 
                 # End line
                 with open(vir_dir+"/"+global_vf_matrix_file, "a") as matrix_file:
-                matrix_file.write("Coverage >= " + 
-                                    str(cfg.config["virulence_genes"]["abricate"]["mincov"]) +
-                                    " % and identity >= " +
-                                    str(cfg.config["virulence_genes"]["abricate"]["minid"]) + 
-                                    " % on each sample for considering a virulence gene as present.")
+                    matrix_file.write("Coverage >= " + 
+                                      str(cfg.config["virulence_genes"]["abricate"]["mincov"]) +
+                                      " % and identity >= " +
+                                      str(cfg.config["virulence_genes"]["abricate"]["minid"]) + 
+                                      " % on each sample for considering a virulence gene as present.")
 
         # Blast call (Virulence genes)
         proteins_database_name = "inhouse_VFDB.txt"    # This is an output file name
