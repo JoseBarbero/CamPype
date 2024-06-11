b12d80206de470d1fdabc8ad7d3ca4b7998673b1
JoseBarbero <jabarbero@ubu.es>
2024-06-11 12:40:51 +0200

    Added aux env to docker

--- create-conda-env.sh
+++ create-conda-env.sh
@@ -2,4 +2,5 @@ export PATH=~/miniconda3/condabin:$PATH
 conda config --append channels conda-forge
 conda config --append channels bioconda
 conda env create -f campype_env.yml
+conda env create -f campype_env_aux.yml
 conda init
